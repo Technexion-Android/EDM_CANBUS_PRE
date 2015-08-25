747a47148296d959771b3ce9e2869865adb9da54
Wig C <wig.cheng@technexion.com>
2015-08-25 17:02:19 +0800

    edm1-cf-imx6: First initial for 'prebuilt' CANBUS app.

---
+++ lib/arm/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libcanbus
+LOCAL_SRC_FILES := libcanbus.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_MODULE_TAGS := optional
+LOCAL_LDLIBS    := -lm -llog
+LOCAL_SHARED_LIBRARIES := libsocketcan_canbus
+
+
+include $(BUILD_PREBUILT)
+include $(call all-makefiles-under,$(LOCAL_PATH))
